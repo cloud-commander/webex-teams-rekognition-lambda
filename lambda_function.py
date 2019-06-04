7cb48da663076f4812592113f4cb364d462a2d30
George Diavatis <35267983+cmsgdiav@users.noreply.github.com>
2019-06-04 12:38:54 +0100

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,3 +1,32 @@
+#!/usr/bin/env python2
+#  -*- coding: utf-8 -*-
+
+"""A Webex Teams bot script that integrates with AWS Rekoginition
+and hosted on AWS Lambda
+
+
+A bot must be created and pointed to this server in the My Apps section of
+https://developer.webex.com.  The bot's Access Token should be added as a
+'WEBEX_TEAMS_ACCESS_TOKEN' environment variable on the Lambda function executing
+this script.
+
+This script only supports Python 2 at this time.
+
+Copyright (c) 2019 George Diavatis.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this
+software and associated documentation files (the "Software"), to deal in the Software
+without restriction, including without limitation the rights to use, copy, modify,
+merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+"""
 
 import json
 import os
